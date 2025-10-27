9908a7086c23702ea6c2a9c5ee03628760989e1c
Raymond Splinter <raymond.splinter@xebia.com>
2025-10-27 09:43:13 +0100

    Fix test

--- cypress/e2e/tools/base64-encoder.cy.ts
+++ cypress/e2e/tools/base64-encoder.cy.ts
@@ -60,16 +60,13 @@ describe("Base64 Encoder Page", () => {
     });
   });
 
-  it("should be able to copy output to clipboard", () => {
+  (Cypress.isBrowser('firefox') ? it.skip : it)("should be able to copy output to clipboard", () => {
     cy.visit("/tools/base64-encoder");
     cy.get("[data-testid='input-textarea']").should("exist").type("Test");
     cy.get("[data-testid='copy-btn']").should("exist").click();
-    // Clipboard is only available for Chromium-based browsers in Cypress tests.
-    if (!Cypress.isBrowser('firefox')) {
-      cy.window().then((win: AUTWindow) => {
-        win.navigator.clipboard.readText().then((text: string) => expect(text).to.eq('VGVzdA=='));
-      });
-    }
+    cy.window().then((win: AUTWindow) => {
+      win.navigator.clipboard.readText().then((text: string) => expect(text).to.eq('VGVzdA=='));
+    });
   });
 
   it("should be able to clear input", () => {
