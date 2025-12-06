5b4eaf3a8726d4957b6e11aeaae6fb2c547cfddf
RHSplinter <60773832+RHSplinter@users.noreply.github.com>
2025-12-06 21:53:35 +0100

    Several minor improvements (#67)
    * Removed SSR
    
    * Removed SSR
    
    * Fixed hljs warning in tests
    
    * Fixed hljs warning in tests
    
    * Add code coverage to Step Summary
    
    * Add code coverage and test results to Step Summary
    
    * Use Karmin junit reporter to display test report in actions
    
    * Add suite to karma report
    
    * Change reporter style
    
    * Change reporter style
    
    * Upload code coverage report
    
    * Upload code coverage report
    
    * Upload code coverage report
    
    * Update job summary
    
    * Update job summary
    
    * Update job summary

---
+++ test-setup.ts
@@ -0,0 +1,9 @@
+// Mock hljs for ngx-highlight-js in tests
+(window as any).hljs = {
+  highlightAll: () => { },
+  highlightElement: () => { },
+  configure: () => { },
+  listLanguages: () => [],
+  getLanguage: () => ({}),
+  highlight: () => ({ value: '' }),
+};
