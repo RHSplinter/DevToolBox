a3e218a8e6ec12a391eb9a2376d794a85865bde3
Raymond Splinter <raymond.splinter@xebia.com>
2025-12-06 21:48:32 +0100

    Update job summary

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -29,7 +29,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'cobertura' }
+        { type: 'cobertura' },
       ]
     },
     reporters: ['progress', 'junit'],
