d20b94f93e1b674886f1c8423e13c141039bd1f4
Raymond Splinter <raymond.splinter@xebia.com>
2025-12-06 16:42:59 +0100

    Update Angular Material to v21

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import {ApplicationConfig} from "@angular/core";
-import {provideRouter, TitleStrategy} from "@angular/router";
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter, TitleStrategy } from "@angular/router";
 
-import {routes} from "./app.routes";
-import {PageTitleStrategy} from "./strategy/page-title.strategy";
-import {HIGHLIGHTJS_CONFIG, HighlightJsConfig} from "ngx-highlight-js"
+import { routes } from "./app.routes";
+import { PageTitleStrategy } from "./strategy/page-title.strategy";
+import { HIGHLIGHTJS_CONFIG, HighlightJsConfig } from "ngx-highlight-js"
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     {
       provide: TitleStrategy,
       useClass: PageTitleStrategy
